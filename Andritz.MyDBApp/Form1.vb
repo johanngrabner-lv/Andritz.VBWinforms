51b2a42a8aa108c5476a644caa76f35648789657
Johann Grabner <jgrabner@business-tuning.at>
2021-02-25 11:29:36 +0100

    SQLConnection

--- Andritz.MyDBApp/Form1.vb
+++ Andritz.MyDBApp/Form1.vb
@@ -1,5 +1,13 @@
-﻿Public Class Form1
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
     Private Sub btnADO_Click(sender As Object, e As EventArgs) Handles btnADO.Click
+        Dim conString As String
+        conString = "server=.;initial catalog=dbAndritz2021;integrated security=true" ' aktuell angemeldeter Benutzer - Studen6
+        Dim con As New SqlConnection(conString)
+        con.Open()
+
+
 
     End Sub
 End Class
